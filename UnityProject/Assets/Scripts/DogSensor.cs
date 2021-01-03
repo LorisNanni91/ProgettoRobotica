2115a183cb2ad1ef87f99a2176c60bfaea9514f4
Loris Nanni <loris.nanni@student.univaq.it>
2021-01-03 17:05:34 +0100

    project clening

---
+++ UnityProject/Assets/Scripts/DogSensor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System;
+
+public class DogSensor : MonoBehaviour
+{
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.gameObject);
+    }
+
+}
