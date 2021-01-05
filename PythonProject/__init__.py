cac8c564d011cfa45df998401e724149f09205b2
Jessica <jessica.giancola@student.univaq.it>
2021-01-05 21:53:07 +0100

    prova

--- PythonProject/__init__.py
+++ PythonProject/__init__.py
@@ -12,14 +12,17 @@ print(cane.useBrain().useMemory().getWorld())
 
 # i = 0
 #
-# while True:
-#     S.sock.SendData('Sent from Python: ' + str(i)) # Send this string to other application
+while True:
+
+
+    S.sock.SendData("ciao unity")
+   # S.sock.SendData('Sent from Python: ') # Send this string to other application
 #     i += 1
 #
-#     data = S.sock.ReadReceivedData() # read data
-#
-#     if data != None: # if NEW data has been received since last ReadReceivedData function call
-#         print(data) # print new received data
+    data = S.sock.ReadReceivedData() # read data
+
+    if data != None: # if NEW data has been received since last ReadReceivedData function call
+        print(data) # print new received data
 #
 #     S.time.sleep(1)
 
---
+++ UnityProject/Assets/Scripts/Avvio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avvio : MonoBehaviour
+{
+    [SerializeField]
+    GameObject piano;
+    [SerializeField]
+    UdpSocket connessione;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 dimensionepiano = new Vector3();
+        dimensionepiano = piano.GetComponent<BoxCollider>().size;
+        Debug.Log(dimensionepiano);
+        string stringa = dimensionepiano.ToString();
+        Debug.Log(stringa);
+        string stringina = "ciao";
+        connessione.SendData(stringina);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
